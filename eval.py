5de9be1583a202fba99858914afd95bd38f13c53
Jianzhi Shen <116665077+jiseshen@users.noreply.github.com>
2024-09-22 11:00:39 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -4,33 +4,34 @@ from torch.utils.data import DataLoader
 
 def contrastive_eval(model, loader_a, loader_b, batch_size):
   model.eval()
-  all_embeddings_a = []
-  all_embeddings_b = []
+  all_emb_a = []
+  all_emb_b = []
   with torch.no_grad():  
     for batch in dataloader_a:
-      embeddings = model(batch)
-      all_embeddings_a.append(embeddings)
+      emb = model.emb_a(batch)
+      all_emb_a.append(embeddings)
 
     for batch in dataloader_b:
-      embeddings = model(batch)
-      all_embeddings_b.append(embeddings)
+      emb = model.emb_b(batch)
+      all_emb_b.append(embeddings)
 
-  all_embeddings_a = torch.cat(all_embeddings_a, dim=0)
-  all_embeddings_b = torch.cat(all_embeddings_b, dim=0)
-  sim_matrix = F.cosine_similarity(all_embeddings_a, all_embeddings_b, dim=-1)
-  retrieval_metric = {k: topk_accuracy(sim_matrix, k) for k in [1, 5, 10]}
+  all_emb_a = torch.cat(all_embeddings_a, dim=0)
+  all_emb_b = torch.cat(all_embeddings_b, dim=0)
+  retrieval_metric = {k: topk_accuracy(all_emb_a, all_emb_b, k) for k in [1, 5, 10]}
   return retrieval_metric
   
 
-def topk_accuracy(sim_matrix, k=10):
+def topk_accuracy(emb_a, emb_b, k=10):
   _, topk_indices_a = torch.topk(similarity_matrix, k, dim=1)
   _, topk_indices_b = torch.topk(similarity_matrix, k ,dim=0)
   correct_a = 0
   correct_b = 0
-  for i in range(sim_matrix.size(0)):
-    if i in topk_indices[i].tolist():
+  for i in range(emb_a.size(0)):
+    _, topk_indices = torch.topk(F.cosine_similarity(emb_a[i], emb_b), k)
+    if i in topk_indices.tolist():
       correct_a += 1
   for i in range(sim_matrix.size(1)):
-    if i in topk_indices[:,i].tolist():
+    _, topk_indices = torch.topk(F.cosine_similarity(emb_b[i], emb_a), k)
+    if i in topk_indices.tolist():
       correct_b += 1
   return correct_a / sim_matrix.size(0), correct_b / sim_matrix.size(1)
